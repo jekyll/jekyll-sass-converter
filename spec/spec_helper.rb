cd9c3330e7ad29b301c7cff0ab97e0ffc23c371f
Parker Moore <parkrmoore@gmail.com>
2014-02-24 19:23:18 -0500

    Initial scaffolding for tests.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'jekyll-sass-converter'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+end
