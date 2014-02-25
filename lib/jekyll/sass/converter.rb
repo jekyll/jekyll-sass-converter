66777397480d41fc1f63423dbbc514539987c93c
Parker Moore <parkrmoore@gmail.com>
2014-02-24 19:04:02 -0500

    Initial import.

---
+++ lib/jekyll/sass/converter.rb
@@ -0,0 +1,9 @@
+require "jekyll/sass/converter/version"
+
+module Jekyll
+  module Sass
+    module Converter
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/sass/converter/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Sass
+    module Converter
+      VERSION = "0.0.1"
+    end
+  end
+end
